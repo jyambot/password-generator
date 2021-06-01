d4db066650897635a6954261486a02941ef94461
jyambot <jerome.yambot@gmail.com>
2021-05-31 21:21:38 -0700

    Switched from using a string to an array of strings. Sequence of character types now randomized. README written.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # password-generator
-Generates a random password after choosing some options via prompts
+<h2>To generate a password:</h2>
+    - Click the [Generate Password] button <br>
+    - Enter a desired length for your password between 8 and 128 characters <br>
+    - The proceeding four prompts will ask about character types. If you would like 
+      to add that type of character to your generated password, click OK. Otherwise, 
+      click Cancel to move to the next prompt. <br>
+    - Once the conditions have been met, a password will be generated inside the box. <br>
+    - If none of the character types are chosen, you will be prompted to start over. <br>
+    
+<br>
+<h3>Update History:</h3>
+<b>5/29:</b> Got the password generator to work on the console. I saw a YouTube video
+      on making a password generator, which gave me the idea to add each character type
+      in sequence. This guarantees that each of the selected character types is added
+      to the generated password. <br>
+      Currently, the generator works by randomly choosing a character in the sequence
+      and concatenating the character to a string. The problem with this design is that
+      the characters follow a sequence and are not actually randomized. So, for example:
+      if you've chosen to have lower case letters, numbers, and symbols in your password,
+      the randomly generated password will follow a sequence of: <br> 
+      
+<p align="center">
+'lower case' - 'number' - 'symbol'
+</p>
+      until the password reaches the defined length.
+
+<br>
+<b>5/31:</b> Reworked the code so that the password is generated as an array where each 
+      new character is pushed to the end. I felt like this way I could more easily shuffle
+      the initially generated password so I could randomize the sequence of character types.
+      In order to randomize the array, I used the Durstenfeld Shuffle algorithm which
+      iterates through each element and swaps it with an element from a randomly chosen spot.
--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 var pwLength, upCase, loCase, number, spChar, allChar;
 var index;
-var pWord = "";
+var pWord = [];
+var pwShuffle = [];
 
 
 // Arrays for each character option
@@ -22,77 +23,73 @@ function getLength() {
 }
 
 function lowerCase() {
-  loCase = confirm("Would you like lower case letters in your password?")
+  loCase = confirm("Would you like lower case letters in your password?");
   return loCase;
 }
 
 function upperCase() {
-  upCase = confirm("Do you want upper case letters in your password?")
+  upCase = confirm("Do you want upper case letters in your password?");
   return upCase;
 }
 
 function numerals() {
-  number = confirm("Would you like numbers in your password?")
+  number = confirm("Would you like numbers in your password?");
   return number;
 }
 
 function specChar() {
-  spChar = confirm("Would you like special characters in your password?")
+  spChar = confirm("Would you like special characters in your password?");
   return spChar;
 }
 
-// getLength();
-// console.log(pwLength);
-// lowerCase();
-// console.log(loCase);
-// upperCase();
-// console.log(upCase);
-// numerals();
-// console.log(number);
-// specChar();
-// console.log(spChar);
 
-
-
-// Simple Password Generator
+// Password Generator: Characters are random, but character type is sequenced.
 function generatePassword() {
   getLength();
   lowerCase();
   upperCase();
   numerals();
   specChar();
+  if(!loCase && !upCase && !number && !spChar) {
+    alert("You must choose at least one character type! Click OK to restart.");
+    generatePassword();
+  }
   for (var i = 0; i < pwLength;) {
     if(i == pwLength) {break}
     if(loCase) {
-      pWord += lcase[Math.floor(Math.random()*(lcase.length))];
-      // console.log(pWord);
+      pWord.push(lcase[Math.floor(Math.random()*(lcase.length))]);
       i++;
     }
     if(i == pwLength) {break}
     if(upCase) {
-      pWord += ucase[Math.floor(Math.random()*(ucase.length))];
-      // console.log(pWord);
+      pWord.push(ucase[Math.floor(Math.random()*(ucase.length))]);
       i++;
     }
     if(i == pwLength) {break}
     if(number) {
-      pWord += nums[Math.floor(Math.random()*(nums.length))];
-      // console.log(pWord);
+      pWord.push(nums[Math.floor(Math.random()*(nums.length))]);
       i++;
     }
     if(i == pwLength) {break}
     if(spChar) {
-      pWord += schar[Math.floor(Math.random()*(schar.length))];
-      // console.log(pWord);
+      pWord.push(schar[Math.floor(Math.random()*(schar.length))]);
       i++;
     }
-  }   
-  return pWord;
+  }
+  shuffle();
+  var passWord = pwShuffle.join('');
+  return passWord;
 }
 
-// generatePassword();
-// console.log(pWord);
-// console.log(pWord.length);
+// Shuffle the generated password, then concatenate into a string.
+function shuffle() {
+  pwShuffle = pWord.slice();
+  for (var j = pwLength - 1; j > 0; j--) {
+    let k = Math.floor(Math.random()*(j + 1));
+    [pwShuffle[j], pwShuffle[k]] = [pwShuffle[k], pwShuffle[j]];
+  }
+  return pwShuffle;
+}
 
 
 // Write password to the #password input
@@ -103,6 +100,5 @@ function writePassword() {
 }
 
 
-
-// // Add event listener to generate button
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
